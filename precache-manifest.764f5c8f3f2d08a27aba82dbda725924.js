5cb623673e09e8245a06b2d9d152c6995721d134
franpog859 <franciszek.pog@gmail.com>
2019-11-24 11:59:06 +0100

    Updates

---
+++ precache-manifest.764f5c8f3f2d08a27aba82dbda725924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8778c393c8bd759663d354fe33ba6931",
+    "url": "/books-exchange/index.html"
+  },
+  {
+    "revision": "5d4d8b4e775a77ef2dd2",
+    "url": "/books-exchange/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8d1bf6bb6d7ef710e211",
+    "url": "/books-exchange/static/js/2.2c214d8b.chunk.js"
+  },
+  {
+    "revision": "5d4d8b4e775a77ef2dd2",
+    "url": "/books-exchange/static/js/main.aecf96f1.chunk.js"
+  },
+  {
+    "revision": "7dd7f903f80da7993b70",
+    "url": "/books-exchange/static/js/runtime-main.4c02f6be.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/books-exchange/static/media/logo.25bf045c.svg"
+  }
+]);
